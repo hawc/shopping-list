ba815f192a15c6b9fc04d07df764efb18bafb408
Hendrik Wichern <git@hendrikwichern.de>
2023-10-08 17:54:38 +0200

    added data handling

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,7 @@ import { defineConfig, squooshImageService } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 
 export default defineConfig({
+  output: 'server',
   image: {
     service: squooshImageService(),
   },
