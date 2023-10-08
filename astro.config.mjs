8dfa4aea8c858652d36399d596f05da79c3dee56
Hendrik Wichern <git@hendrikwichern.de>
2023-10-08 17:57:07 +0200

    added netlify plugin

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,13 @@
 import { defineConfig, squooshImageService } from 'astro/config';
-
 import tailwind from '@astrojs/tailwind';
+import netlify from "@astrojs/netlify/functions";
 
+// https://astro.build/config
 export default defineConfig({
   output: 'server',
   image: {
-    service: squooshImageService(),
+    service: squooshImageService()
   },
-  integrations: [tailwind()]
+  integrations: [tailwind()],
+  adapter: netlify()
 });
